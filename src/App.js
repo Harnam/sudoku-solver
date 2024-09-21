d8e51c5dec9c25bfbe27705ec36b44eba1fd06c6
Harnam Singh <harnamsinghsandhu04@gmail.com>
2024-09-21 17:51:59 +0530

    import export function and update ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -6,12 +6,12 @@ function App() {
   const [vals, setVals] = useState(
     JSON.parse(JSON.stringify(Array(3).fill(Array(3).fill(Array(3).fill(Array(3).fill(null))))))
   );
+  useEffect(updateUI, [vals]);
   
   function handleValue (bigRow, bigSquare, row, square, val) {
     const newVals = vals.slice();
     newVals[bigRow][bigSquare][row][square] = val;
     setVals(newVals);
-    buttonEnable(checkValidity(bigRow, bigSquare, row, square, val));
   }
 
   function buttonEnable(val) {
@@ -41,10 +41,45 @@ function App() {
     return true;
   }
 
+  function updateUI() {
+    let buttonEnabled = true;
+    for(let i = 0; i < 3; i++) {
+      for(let j = 0; j < 3; j++) {
+        for(let k = 0; k < 3; k++) {
+          for(let l = 0; l < 3; l++) {
+            if(vals[i][j][k][l]){
+              console.log(i,j,k,l)
+              document.getElementById("square"+ i + "" + j + "" + k + "" + l).value = vals[i][j][k][l];
+              let isValid = (checkValidity(i, j, k, l, vals[i][j][k][l]));
+              buttonEnabled = buttonEnabled && isValid;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  function importData() {
+    const impexp = document.getElementById("impexp");
+    const newVals = JSON.parse(impexp.value);
+    setVals(newVals);
+    updateUI();
+  }
+
+  function exportData() {
+    const impexp = document.getElementById("impexp");
+    impexp.value = JSON.stringify(vals);
+  }
+
   return (
     <div>
       <Board handleValue={ handleValue } />
       <button id="solvebtn">Solve</button>
+      <div>
+        <input id="impexp" type="text"></input>
+        <button id="impbtn" onClick={ importData }>Import</button>
+        <button id="expbtn" onClick={ exportData }>Export</button>
+      </div>
     </div>
   );
 }
